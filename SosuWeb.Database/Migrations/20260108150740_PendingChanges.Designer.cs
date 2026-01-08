4d782264a3f82c578d9978d4bae066ca4fad0487
Shahzod <shachzod2004@gmail.com>
2026-01-08 16:08:02 +0100

    Add new migrations

--- SosuWeb.Database/Migrations/20260108150740_PendingChanges.Designer.cs
+++ SosuWeb.Database/Migrations/20260108150740_PendingChanges.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SosuWeb.Database;
@@ -11,9 +12,11 @@ using SosuWeb.Database;
 namespace SosuWeb.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class RendererContextModelSnapshot : ModelSnapshot
+    [Migration("20260108150740_PendingChanges")]
+    partial class PendingChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
